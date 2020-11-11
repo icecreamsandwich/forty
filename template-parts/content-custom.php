c73b56223eeafb30dd05e422d8f1f48b1d127626
crazyRubix <muneebkt@gmail.com>
2020-11-11 17:43:12 +0530

    removing page titles

--- template-parts/content-custom.php
+++ template-parts/content-custom.php
@@ -84,54 +84,6 @@ $sec3_link = $custom_group['section3-link'];
                     // Do something...
                     endif;
                     ?>
-                    <!--  <section>
-                        <a href="generic.html" class="image">
-                            <img src="images/pic08.jpg" alt="" data-position="center center" />
-                        </a>
-                        <div class="content">
-                            <div class="inner">
-                                <header class="major">
-                                    <h3>Orci maecenas</h3>
-                                </header>
-                                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                                <ul class="actions">
-                                    <li><a href="generic.html" class="button">Learn more</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                    <section>
-                        <a href="generic.html" class="image">
-                            <img src="images/pic09.jpg" alt="" data-position="top center" />
-                        </a>
-                        <div class="content">
-                            <div class="inner">
-                                <header class="major">
-                                    <h3>Rhoncus magna</h3>
-                                </header>
-                                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                                <ul class="actions">
-                                    <li><a href="generic.html" class="button">Learn more</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-                    <section>
-                        <a href="generic.html" class="image">
-                            <img src="images/pic10.jpg" alt="" data-position="25% 25%" />
-                        </a>
-                        <div class="content">
-                            <div class="inner">
-                                <header class="major">
-                                    <h3>Sed nunc ligula</h3>
-                                </header>
-                                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                                <ul class="actions">
-                                    <li><a href="generic.html" class="button">Learn more</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section> -->
                 </section>
 
                 <!-- Three -->
