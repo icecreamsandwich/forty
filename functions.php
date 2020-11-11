964bb9f0c5c9feeb3f026d0ee2bd220ec114acbf
crazyRubix <muneebkt@gmail.com>
2020-11-11 13:01:29 +0530

    fix footer social icons, footer widget

--- functions.php
+++ functions.php
@@ -133,6 +133,17 @@ function forty_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer', 'forty' ),
+			'id'            => 'footer-1',
+			'description'   => esc_html__( 'Add footer widgets here.', 'forty' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action( 'widgets_init', 'forty_widgets_init' );
 
